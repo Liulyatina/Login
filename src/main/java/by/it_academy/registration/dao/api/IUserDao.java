dda3c1a4ad193d6ebaa9c044eaee6ac5012443aa
Liulyatina <Roma.mihal812@gmail.com>
2024-03-11 10:26:49 +0300

    удалил ненужный instance

--- src/main/java/by/it_academy/registration/dao/api/IUserDao.java
+++
@@ -1,9 +0,0 @@
-package by.it_academy.registration.dao.api;
-
-import by.it_academy.registration.controller.User;
-
-public interface IUserDao {
-    void addUser(User user);
-
-    User getUserByLogin(String login);
-}
